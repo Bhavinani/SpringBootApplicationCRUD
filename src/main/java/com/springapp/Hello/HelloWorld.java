2a41643985052b4493da51828b8e8cc94f6a2e14
LENOVO <LENOVO@DESKTOP-EUMA5GN>
2024-06-27 16:20:59 +0530

    Spring Boot Web2 : My Second commit.

--- src/main/java/com/springapp/Hello/HelloWorld.java
+++ src/main/java/com/springapp/Hello/HelloWorld.java
@@ -8,8 +8,10 @@ public class HelloWorld implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		int a =20;
 		
 		System.out.println("This is my First Spring Boot Application with GitHub....");
+		System.out.println("The a value id:::" +a);
 	}
 	
 	
